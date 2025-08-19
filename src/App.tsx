45db55e7db62376b8ddf2ff38003f6ce9035798b
Swarnajith Fernando <swarnajith@gmail.com>
2025-08-19 14:22:21 +1000

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  const showAside = useBreakpointValue({ base: false, lg: true });
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      {showAside && (
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      )}
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
