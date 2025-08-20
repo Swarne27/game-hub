5183f10143539a5a4a19f331fc1e3459e32120a4
Swarnajith Fernando <swarnajith@gmail.com>
2025-08-20 11:56:16 +1000

    Implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
+
 import NavBar from "./components/NavBar";
 
 function App() {
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,17 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Text, Button } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import { useColorMode } from "@/components/ui/color-mode";
 
 function NavBar() {
+  const { toggleColorMode } = useColorMode();
+
   return (
     <HStack>
       <Image src={logo} boxSize="60px" />
       <Text>NavBar</Text>
+      <Button variant="outline" onClick={toggleColorMode}>
+        Toggle Mode
+      </Button>
     </HStack>
   );
 }
